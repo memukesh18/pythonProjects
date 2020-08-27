10d145d2f31999a7fce713c2f20bd1c11485f69f
Mukesh K <me.mukesh18@gmail.com>
2020-08-27 12:19:05 +0530

    Added verifyPassword.py to LearnPython

---
+++ LearnPython/verifyPassword.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+passwordFile = open('./Files/secretPwdFile.txt')
+secretPassword = passwordFile.read()
+print('Enter your password.')
+typedPassword = input()
+
+if typedPassword == secretPassword:
+    print("Access Granted!!")
+    if typedPassword == '12345':
+        print("That password is one that an idiot puts on their luggage.")
+else:
+    print("Access Denied!!")
+
+
+
